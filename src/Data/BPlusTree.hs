0375550b11a6eae5ba318fd069aa1d7288d4ca64
Jason Dagit <dagit@codersbase.com>
2010-09-07 05:23:12 +0000

    bit of clean up and new implementation

--- src/Data/BPlusTree.hs
+++ src/Data/BPlusTree.hs
@@ -1,17 +1,59 @@
+{-# LANGUAGE GADTs, EmptyDataDecls #-}
 module Data.BPlusTree where
 
+-- import qualified System.IO as S ( Handle, IOMode(..), hSeek,
+--                                  hTell, SeekMode(..) )
+import qualified System.IO as S ( Handle, IOMode(..) )
+import Foreign ( ForeignPtr )
 import Data.Map ( Map )
+import qualified Data.Map as M ( empty, toList, lookup, elemAt
+                               , size, elemAt, split )
+import Prelude hiding ( lookup )
+-- import Control.Applicative ( (<$>) )
 
--- | The unconventional structure of the B+ Tree means
--- there are internal nodes inside the tree that are different
--- than the leaf nodes which only appear at the deepest evel of
--- the tree.  We use a type structure that enforces the invariant.
-data BPlus k a = L (Leaf k a)
-               | N (BPlus k (Node k a))
-data Leaf k a = Leaf { elements :: Map k a -- | TODO: change this to Data.Vector?
-                     , next     :: Maybe (Leaf k a)
-                     }
-data Node k a = Node { parent  :: Node k a
-                     , lesser  :: [Node k a] -- | length between n and 2n (inclusive)
-                     , greater :: [Node k a] -- | length between n and 2n (inclusive)
-                     }
+
+-- | B+ Tree
+data Leaf
+data Node
+
+data BPlus t k a where
+  BPLeaf :: Map k a             -> Maybe (BPlus Leaf k a) -> BPlus Leaf k a
+  BPNode :: Map k (BPlus t k a) -> BPlus Node k a
+
+-- | Infrastructure for managing B+ Tree on disk
+-- Not yet used or written.
+newtype Offset a = BP Integer
+
+data Location = MemoryLocation
+              | FileLocation FilePath S.IOMode
+
+data Handle a = FileHandle S.Handle
+              | MemoryHandle Integer (ForeignPtr a)
+{-
+
+seek :: Handle a -> Offset a -> IO ()
+seek (FileHandle h)   (BP i) = S.hSeek h S.AbsoluteSeek i
+seek (MemoryHandle h) (BP i) = 
+tell :: Handle a -> IO (Offset a)
+tell (FileHandle h) = BP <$> S.hTell h
+
+-}
+
+-- | Pure API
+empty :: BPlus Leaf k a
+empty = BPLeaf M.empty Nothing
+
+toList :: BPlus Leaf k a -> [(k, a)]
+toList (BPLeaf m Nothing)  = M.toList m
+toList (BPLeaf m (Just l)) = M.toList m ++ toList l
+
+lookup :: Ord k => k -> BPlus t k a -> Maybe a
+lookup k (BPLeaf m _) = M.lookup k m
+lookup k (BPNode cs)
+  | isEmpty cs      = Nothing -- Odd case
+  | isEmpty l       = lookup k (snd (M.elemAt 0 cs))
+  | isEmpty r       = Nothing -- k too large
+  | otherwise       = lookup k (snd (M.elemAt 0 r))
+  where
+  (l, r)    = M.split k cs
+  isEmpty m = M.size m == 0
